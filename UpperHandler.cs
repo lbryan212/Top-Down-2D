73bbb836e20e486e26b9b7f597976acbd00e98ec
lbryan212 <lbryan212@gmail.com>
2018-11-01 14:02:35 -0400

    Create UpperHandler.cs

---
+++ UpperHandler.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpperHandler : MonoBehaviour {
+
+
+    public bool Moving;
+    public bool Primary;
+    public bool Secondary;
+    public bool Knife;
+
+
+
+    public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
+	}
+}
